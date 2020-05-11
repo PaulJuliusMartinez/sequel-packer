2fb5ebbcd9451af1733f6c775b97ebb74cdf8ef1
Paul Julius Martinez <pauljuliusmartinez@gmail.com>
2020-05-10 22:40:18 -0700

    Support field(name, &block) and field(&block).

--- lib/sequel/packer.rb
+++ lib/sequel/packer.rb
@@ -1,11 +1,38 @@
 module Sequel
   class Packer
+    # For invalid arguments provided to the field class method.
+    class FieldArgumentError < ArgumentError; end
+
     def self.inherited(subclass)
       subclass.instance_variable_set(:@fields, [])
     end
 
-    def self.field(field_name)
-      @fields << field_name
+    def self.field(field_name=nil, &block)
+      if block
+        arity = block.arity
+
+        if field_name && (arity != 1 && arity != -1)
+          raise(
+            FieldArgumentError,
+            "The block used to define :#{field_name} must accept exactly " +
+              'one argument.',
+          )
+        end
+
+        if !field_name && arity != 2
+          raise(
+            FieldArgumentError,
+            'When passing an arbitrary block to Sequel::Packer::field, the ' +
+              'block must accept exactly two arguments: the model and the ' +
+              'partially packed hash.',
+          )
+        end
+      end
+
+      @fields << {
+        name: field_name,
+        block: block,
+      }
     end
 
     def fields
@@ -15,9 +42,22 @@ module Sequel
     def pack(dataset)
       dataset.map do |model|
         h = {}
-        fields.each do |field_name|
-          h[field_name] = model.send(field_name)
+
+        fields.each do |field_options|
+          field_name = field_options[:name]
+          block = field_options[:block]
+
+          if block
+            if field_name
+              h[field_name] = block.call(model)
+            else
+              block.call(model, h)
+            end
+          else
+            h[field_name] = model.send(field_name)
+          end
         end
+
         h
       end
     end
--- test/sequel/packer_test.rb
+++ test/sequel/packer_test.rb
@@ -1,24 +1,50 @@
 require "test_helper"
 
 class Sequel::PackerTest < Minitest::Test
+  # Run tests in Sequel transaction.
+  def run(*args, &block)
+    DB.transaction(:rollback=>:always, :auto_savepoint=>true) {super}
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Sequel::Packer::VERSION
   end
 
-  class UserPacker < Sequel::Packer
+  class BasicFieldPacker < Sequel::Packer
     field :id
     field :name
   end
 
-  def test_it_packs_basic_models
+  def test_it_packs_basic_fields
     paul = User.create(name: 'Paul')
     julius = User.create(name: 'Julius')
 
-    packed_users = UserPacker.new.pack(User.order(:id))
+    packed_users = BasicFieldPacker.new.pack(User.order(:id))
 
     assert_equal paul.id, packed_users[0][:id]
     assert_equal paul.name, packed_users[0][:name]
     assert_equal julius.id, packed_users[1][:id]
     assert_equal julius.name, packed_users[1][:name]
+  end
+
+  class BlockFieldPacker < Sequel::Packer
+    field(:id) {|model| model.id}
+    field(:name, &:name)
+
+    field do |model, hash|
+      hash[:id_from_block] = model.id
+      hash[:name_from_block] = model.name
+    end
+  end
+
+  def test_it_packs_fields_defined_via_blocks
+    paul = User.create(name: 'Paul')
+
+    packed_user = BlockFieldPacker.new.pack(User.dataset)[0]
+
+    assert_equal paul.id, packed_user[:id]
+    assert_equal paul.name, packed_user[:name]
+    assert_equal paul.id, packed_user[:id_from_block]
+    assert_equal paul.name, packed_user[:name_from_block]
   end
 end
