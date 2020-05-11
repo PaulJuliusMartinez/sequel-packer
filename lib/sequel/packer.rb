7dcea8379e018f3d16a0ee7b2e7f0f508612a1a1
Paul Julius Martinez <pauljuliusmartinez@gmail.com>
2020-05-11 00:23:16 -0700

    Add significant validation to calls to Sequel::Packer::field.

--- lib/sequel/packer.rb
+++ lib/sequel/packer.rb
@@ -2,16 +2,57 @@ module Sequel
   class Packer
     # For invalid arguments provided to the field class method.
     class FieldArgumentError < ArgumentError; end
+    # Must declare a model with `model MyModel` before calling field.
+    class ModelNotYetDeclaredError < StandardError; end
 
     def self.inherited(subclass)
       subclass.instance_variable_set(:@fields, [])
     end
 
-    def self.field(field_name=nil, &block)
+    def self.model(klass)
+      if !(klass < Sequel::Model)
+        fail(
+          ArgumentError,
+          'model declaration must be a subclass of Sequel::Model',
+        )
+      end
+
+      fail ArgumentError, 'model already declared' if @model
+
+      @model = klass
+    end
+
+    def self.field(field_name=nil, packer_class=nil, &block)
+      fail ModelNotYetDeclaredError if !@model
+
+      # This check applies to all invocations:
+      if field_name && !field_name.is_a?(Symbol) && !field_name.is_a?(String)
+        raise(
+          FieldArgumentError,
+          'Field name passed to Sequel::Packer::field must be a Symbol or ' +
+            'a String.',
+        )
+      end
+
       if block
+        # If the user passed a block, we'll assume they either want:
+        #     field :foo {|model| ...}
+        # or  field {|model, hash| ...}
+        #
+        if packer_class
+          raise(
+            FieldArgumentError,
+            'When passing a block to Sequel::Packer::field, either pass the ' +
+              'name of field as a single argument (e.g., field(:foo) ' +
+              '{|model| ...}), or nothing at all to perform arbitrary ' +
+              'modifications of the final hash (e.g., field {|model, hash| ' +
+              '...}).',
+          )
+        end
+
         arity = block.arity
 
-        if field_name && (arity != 1 && arity != -1)
+        if field_name && arity != 1 && arity != -1
           raise(
             FieldArgumentError,
             "The block used to define :#{field_name} must accept exactly " +
@@ -27,16 +68,67 @@ module Sequel
               'partially packed hash.',
           )
         end
+      else
+        # In this part of the if, block is not defined
+
+        if !field_name
+          # Note that this error isn't technically true, but usage of the
+          # field {|model, hash| ...} variant is likely pretty rare.
+          raise(
+            FieldArgumentError,
+            'Must pass a field name to Sequel::Packer::field.',
+          )
+        end
+
+        if packer_class
+          if !@model.associations.include?(field_name)
+            raise(
+              FieldArgumentError,
+              'Passing multiple arguments to Sequel::Packer::field ' +
+                'is used to serialize associations with designated ' +
+                "packers, but the association #{field_name} does not " +
+                "exist on #{@model}.",
+            )
+          end
+
+          if !(packer_class < Sequel::Packer)
+            raise(
+              FieldArgumentError,
+              'When declaring the serialization behavior for an ' +
+                'association, the second argument must be a Sequel::Packer ' +
+                "subclass. #{packer_class} is not a subclass of " +
+                'Sequel::Packer.',
+            )
+          end
+
+          association_model =
+            @model.association_reflections[field_name].associated_class
+          packer_class_model = packer_class.instance_variable_get(:@model)
+
+          if !(association_model < packer_class_model)
+            raise(
+              FieldArgumentError,
+              "Model for association packer (#{packer_class_model}) " +
+                "doesn't match model for the #{field_name} association " +
+                "(#{association_model})",
+            )
+          end
+        else
+          if @model.associations.include?(field_name)
+            raise(
+              FieldArgumentError,
+              'When declaring a field for a model association, you must ' +
+                'also pass a Sequel::Packer class to use as the second ' +
+                'argument to field.',
+            )
+          end
+        end
       end
 
       @fields << {
         name: field_name,
         block: block,
       }
-    end
-
-    def fields
-      self.class.instance_variable_get(:@fields)
     end
 
     def pack(dataset)
@@ -60,6 +152,12 @@ module Sequel
 
         h
       end
+    end
+
+    private
+
+    def fields
+      self.class.instance_variable_get(:@fields)
     end
   end
 end
--- test/sequel/packer_test.rb
+++ test/sequel/packer_test.rb
@@ -10,7 +10,103 @@ class Sequel::PackerTest < Minitest::Test
     refute_nil ::Sequel::Packer::VERSION
   end
 
+  class DummyUserPacker < Sequel::Packer
+    model User
+  end
+
+  class DummyPostPacker < Sequel::Packer
+    model Post
+  end
+
+  ############################################
+  # Validation of arguments passed to field. #
+  ############################################
+
+  def test_raises_if_model_not_yet_declared
+    assert_raises(Sequel::Packer::ModelNotYetDeclaredError) do
+      Class.new(Sequel::Packer) do
+        field :id
+      end
+    end
+  end
+
+  def assert_packer_declaration_raises(model_klass, &block)
+    assert_raises(Sequel::Packer::FieldArgumentError) do
+      klass = Class.new(Sequel::Packer)
+      klass.send(:model, model_klass) if model_klass
+      klass.class_eval(&block)
+    end
+  end
+
+  def test_field_name_must_be_a_symbol_or_string
+    err = assert_packer_declaration_raises(User) do
+      field 4
+    end
+    assert_includes err.message, 'Symbol'
+    assert_includes err.message, 'String'
+  end
+
+  def test_block_for_field_doesnt_accept_correct_number_of_arguments
+    err = assert_packer_declaration_raises(User) do
+      field(:user) {}
+    end
+    assert_includes err.message, 'exactly one argument'
+
+    err = assert_packer_declaration_raises(User) do
+      field(:user) {|_, _|}
+    end
+    assert_includes err.message, 'exactly one argument'
+  end
+
+  def test_block_only_doesnt_accept_correct_number_of_arguments
+    err = assert_packer_declaration_raises(User) do
+      field {|_model|}
+    end
+    assert_includes err.message, 'exactly two arguments'
+  end
+
+  def test_association_field_doesnt_specify_packer
+    err = assert_packer_declaration_raises(User) do
+      field :posts
+    end
+    assert_includes err.message, 'must also pass a Sequel::Packer class'
+  end
+
+  def test_association_doesnt_exist
+    err = assert_packer_declaration_raises(User) do
+      field :recent_posts, DummyPostPacker
+    end
+    assert_includes err.message, 'association recent_posts does not exist'
+  end
+
+  def test_association_packer_class_isnt_a_packer
+    err = assert_packer_declaration_raises(User) do
+      field :posts, String
+    end
+    assert_includes err.message, 'is not a subclass of Sequel::Packer'
+  end
+
+  def test_association_packer_class_model_doesnt_match_association_model
+    err = assert_packer_declaration_raises(User) do
+      field :posts, DummyUserPacker
+    end
+    assert_includes err.message, "doesn't match model for the posts association"
+  end
+
+  def test_multiple_arguments_with_block
+    err = assert_packer_declaration_raises(User) do
+      field(:posts, DummyPostPacker) {|_model|}
+    end
+    assert_includes err.message, 'passing a block to Sequel::Packer::field'
+  end
+
+  ######################################
+  # Actual Packer functionality tests. #
+  ######################################
+
   class BasicFieldPacker < Sequel::Packer
+    model User
+
     field :id
     field :name
   end
@@ -28,6 +124,8 @@ class Sequel::PackerTest < Minitest::Test
   end
 
   class BlockFieldPacker < Sequel::Packer
+    model User
+
     field(:id) {|model| model.id}
     field(:name, &:name)
 
